ef25a7008f4baf4f80ee479c313262eb290c7be1
Luper95 <luisenriqueperez2605@hotmail.com>
2025-05-16 18:19:42 +0000

    Resuelto

--- src/js/components/Home.jsx
+++ src/js/components/Home.jsx
@@ -1,12 +1,23 @@
 import React from "react";
-
+import NavBarComponent from "./NavBarComponent";
 import FooterComponent from "./FooterComponent";
 import CardComponent from "./CardComponent";
 
 //create your first component
 const Home = () => {
 	return (
-		
+		<>
+			<NavBarComponent />
+			<div className="container">
+				<div className="row row-cols-1 row-cols-lg-4 g-4 py-3">
+					<CardComponent />
+					<CardComponent />
+					<CardComponent />
+					<CardComponent />
+				</div>
+			</div>
+			<FooterComponent />
+		</>
 	);
 };
 
