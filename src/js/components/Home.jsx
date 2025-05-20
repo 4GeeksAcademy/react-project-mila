cadc332f8d5ecbc24de2e5908b3508a05bf9f16e
anagit2025 <pintorosbelys90@gmail.com>
2025-05-20 16:41:31 +0000

    header

--- src/js/components/Home.jsx
+++ src/js/components/Home.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import NavBarComponent from "./NavBarComponent";
 import FooterComponent from "./FooterComponent";
 import CardComponent from "./CardComponent";
+import HeaderComponent from "./HeaderComponent";
 
 //create your first component
 const Home = () => {
@@ -10,6 +11,8 @@ const Home = () => {
 			<NavBarComponent />
 			<div className="container">
 				<div className="row row-cols-1 row-cols-lg-4 g-4 py-3">
+					<HeaderComponent/>
+					
 					<CardComponent />
 					<CardComponent />
 					<CardComponent />
